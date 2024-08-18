795d777f3a5dadcab99589e63212bfe5be91630a
Maks Kom <mr.cheese1213@gmail.com>
2024-08-18 17:54:07 +0200

    just to not lose it

--- defs.py
+++ defs.py
@@ -1,16 +1,7 @@
 import os
 import time
 def startup():
-    print("Welcome, this is Nature helper project")
-    time.sleep(3)
-    print("We will initialize an server for you, also the main site language is Russian")
-    time.sleep(5)
-    print("But it is settable in site itself")
-    time.sleep(3)
-    os.system("copy static/defs_sectime.py defs.py")
-    print("This welcome text will not appear again")
-    time.sleep(2)
-    os.system("cls")
+    print("Welcome text not shown due to non-first time")
     print("Please wait, we are now starting.")
     time.sleep(1)
     os.system("cls")
@@ -20,3 +11,6 @@ def startup():
     print("Please wait, we are now starting...")
     time.sleep(1)
     os.system("cls")
+
+# Do not pay attetion at this symbol below, it is commented to prevent script from crashing
+# 
